2d5261e3651816adbbf2e0fb068760315e3f130f
Shemar Dacosta <dshemar6@gmail.com>
2019-03-11 17:31:45 -0400

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
+const main = document.getElementById('main');
+const input = document.querySelector('input');
+
+main.addEventListener('click', function(event) {
+  alert('I was clicked!');
+});
+
 function addingEventListener() {
+  input.addEventListener('input', function(event) {
+    alert('I was changed!');
+  });
 }
